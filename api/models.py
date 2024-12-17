8d10e50f0039d041087809f28f0e679e1f47748a
AleBenjaaa <alebenj4min@gmail.com>
2024-12-17 18:36:06 -0300

    as

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Medicamento(models.Model):
     nombre = models.CharField(max_length=255)
     descripcion = models.TextField()
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    precio = models.IntegerField()
     stock = models.IntegerField()
 
     def __str__(self):
